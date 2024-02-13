66c00d8d6c568bd4b53a59c9851d61fa1dd06d83
JPMPDEV <john_paul_pangan@dlsu.edu.ph>
2024-02-13 21:48:46 +0800

    Wall

---
+++ src/controller/Wall.java
@@ -0,0 +1,14 @@
+package controller;
+
+public class Wall {
+    int xpos1;
+    int ypos1;
+    int xpos2;
+    int ypos2;
+    public Wall(int x1, int y1, int x2, int y2){
+        xpos1 = x1;
+        ypos1 = y1;
+        xpos2 = x2;
+        ypos2 = y2;
+    }
+}
