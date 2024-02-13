09873bcc9b8b5d2e4af17fecaee9511c16a3c3b0
JPMPDEV <john_paul_pangan@dlsu.edu.ph>
2024-02-13 16:17:48 +0800

    Bounce

--- src/controller/Particle.java
+++ src/controller/Particle.java
@@ -22,8 +22,22 @@ public class Particle {
             xpos += xvel;
             ypos += yvel;
             //update position
+            Bounce()
         }
         while (true);
     }
     
+    void bounce(){
+        //initial border collisions
+        if (xpos+1 > 640 or xpos-1 < -640)//right left collision
+        {
+            xvel *= -1;
+        }
+        if (ypos+1 > 320 || ypos-1 < 320) //top bottom collision
+        {
+            yvel *= -1;
+        }
+        //create 4 checking pixels surrounding the particle
+        //each pixel checks 
+    }
 }
