536a6ab2fce105c659fb8fb3c3fc18ee92c5f793
Eayyu20 <ethan_yu@dlsu.edu.ph>
2024-02-15 18:42:43 +0800

    Fixed Panels

--- src/view/GUI.java
+++ src/view/GUI.java
@@ -2,19 +2,21 @@ package view;
 import javax.swing.*;
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
 import java.awt.HeadlessException;
 import java.awt.color.ColorSpace;
 import controller.Particle;
 import java.awt.Graphics2D;
-import view.DrawingPanel;
+import view.*;
 
 public class GUI {
 
     private JFrame window;
     private DrawingPanel mainpanel;
-    private JPanel canvas;
+    private JPanel sidepanel;
     private JLabel label;
-    private static int SCREEN_WIDTH = 1500;
+    private static int SCREEN_WIDTH = 1550;
     private static int SCREEN_HEIGHT = 720;
 
     public GUI() {
@@ -22,21 +24,24 @@ public class GUI {
         window.setBounds(120, 80, SCREEN_WIDTH, SCREEN_HEIGHT);
         window.setUndecorated(false);
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        window.setLayout(new BorderLayout());
 
         mainpanel = new DrawingPanel();
-        mainpanel.setSize(1280, 720);
+        mainpanel.setPreferredSize(new Dimension(1280, 720));
         mainpanel.setBackground(Color.white);
-        window.add(mainpanel);
+        window.add(mainpanel, BorderLayout.WEST);
         //mainpanel.drawWall(0, 0, 1280, 720, null);
         
-        canvas = new JPanel();
-        canvas.setSize(220, 720);
-        canvas.setBackground(Color.red);
-        window.add(canvas);
+        sidepanel = new JPanel();
+        sidepanel.setPreferredSize(new Dimension(280, 720));
+        sidepanel.setBackground(Color.red);
+        window.add(sidepanel, BorderLayout.EAST);
         
-        label = new JLabel();
-        label.setText("Hello");
-        window.add(label, BorderLayout.CENTER);
+        JTextField angle = new JTextField();
+        angle.setPreferredSize(new Dimension(100,20));
+        angle.setFont(new Font("Consolas", Font.PLAIN, 35));
+        sidepanel.add(angle);
+
     }
 
     public void toggleVisibility(){
