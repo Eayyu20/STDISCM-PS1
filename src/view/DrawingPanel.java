86cbf94f1fa206da54da84a97e3f6293369963d5
JPMPDEV <john_paul_pangan@dlsu.edu.ph>
2024-02-15 18:51:12 +0800

    3 particles
    made particle list, drawing 3 particles

--- src/view/DrawingPanel.java
+++ src/view/DrawingPanel.java
@@ -2,7 +2,7 @@ package view;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import java.util.ArrayList;
 import javax.swing.*;
 import controller.Particle;
 
@@ -10,13 +10,20 @@ import controller.Particle;
 public class DrawingPanel extends JPanel implements ActionListener{
     final int PANEL_WIDTH = 1280;
     final int PANEL_HEIGHT = 720;
-    Particle testp = new Particle(0, 0, 45,10);
+    public ArrayList<Particle> palist;
+    public Particle testp = new Particle(0, 0, 45,10);
+    public Particle tesp = new Particle(50, 20, 70,30);
+    public Particle wasp = new Particle(1270, 710, -60,5);
     Timer timer;
     public float xpos = 0;
     public float ypos = 0;
     DrawingPanel(){
         timer = new Timer(10, this);
         timer.start();
+        palist = new ArrayList<>();
+        palist.add(testp);
+        palist.add(tesp);
+        palist.add(wasp);
     }
     //test
     public void paint(Graphics g){
@@ -26,15 +33,19 @@ public class DrawingPanel extends JPanel implements ActionListener{
         g2d.setColor(Color.black);
         g2d.fillRect(0, 0, 1280, 720);
         g2d.setColor(Color.white);
-        g2d.fillOval(Math.round(testp.getXpos()),Math.round(testp.getYpos()),10,10);
+        for (int i = 0; i<palist.size(); i++){
+            g2d.fillOval(Math.round(palist.get(i).getXpos()),Math.round(palist.get(i).getYpos()),10,10);
+        } 
     }
     @Override
     public void actionPerformed(ActionEvent e){
-        testp.Move();
-        xpos = testp.getXpos();
-        ypos = testp.getYpos();
-        //initial border collisions
-        repaint();
+        //for every particle in the list
+        for (int i = 0; i<palist.size(); i++){
+            palist.get(i).Move();
+            xpos = palist.get(i).getXpos();
+            ypos = palist.get(i).getYpos();
+            repaint();
+        }        
     }
     /*public void drawWall(int x1, int y1, int x2, int y2, Graphics g){
         Graphics2D g2d = (Graphics2D) g;
