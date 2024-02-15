02029e43f2fa30ca039d14695438b39039a96b8b
Eayyu20 <ethan_yu@dlsu.edu.ph>
2024-02-15 22:05:35 +0800

    Updated user input GUI

--- src/view/GUI.java
+++ src/view/GUI.java
@@ -2,21 +2,37 @@ package view;
 import javax.swing.*;
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.HeadlessException;
 import java.awt.color.ColorSpace;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import controller.Particle;
 import java.awt.Graphics2D;
 import view.*;
 
-public class GUI {
+public class GUI extends JFrame implements ActionListener{
 
     private JFrame window;
     private DrawingPanel mainpanel;
     private JPanel sidepanel;
-    private JLabel label;
     private Particle testp;
+
+    private JTextField xInput;
+    private JTextField yInput;
+    private JTextField deltaInput;
+    private JTextField vInput;
+    private JButton submitParticle;
+
+    private JTextField x1Input; 
+    private JTextField y1Input;
+    private JTextField x2Input;
+    private JTextField y2Input;
+    private JButton submitWall; 
+
     private static int SCREEN_WIDTH = 1550;
     private static int SCREEN_HEIGHT = 720;
 
@@ -35,17 +51,100 @@ public class GUI {
         
         sidepanel = new JPanel();
         sidepanel.setPreferredSize(new Dimension(280, 720));
-        sidepanel.setBackground(Color.red);
+        sidepanel.setBackground(Color.gray);
+        sidepanel.setLayout(new BoxLayout(sidepanel, BoxLayout.Y_AXIS));
         window.add(sidepanel, BorderLayout.EAST);
+
+        JLabel addParticleText = new JLabel("Add Particle");
+        addParticleText.setAlignmentX(Component.CENTER_ALIGNMENT);
+        addParticleText.setFont(new Font("Ubuntu", Font.BOLD, 15));
+        // addParticleText.setForeground(new Color(0x315AEB));
+        // addParticleText.setBorder(BorderFactory.createEmptyBorder( 0, 0, 50, 0 ));
+        addParticleText.setPreferredSize(new Dimension(100, 30));
+        sidepanel.add(addParticleText);
         
-        JTextField angle = new JTextField();
-        angle.setPreferredSize(new Dimension(100,20));
-        angle.setFont(new Font("Consolas", Font.PLAIN, 35));
-        sidepanel.add(angle);
+        JLabel xText = new JLabel("x: ");
+        xText.setHorizontalTextPosition(JLabel.CENTER);
+        xText.setVerticalTextPosition(JLabel.TOP);
+        xText.setFont(new Font("Ubuntu", Font.BOLD, 15));
+        xText.setPreferredSize(new Dimension(100, 30));
+        sidepanel.add(xText);
+
+        xInput = new JTextField();
+        xInput.setMaximumSize(new Dimension(100, 30));
+        xInput.setPreferredSize(new Dimension(100,20));
+        xInput.setFont(new Font("Ubuntu", Font.PLAIN, 12));
+        xInput.setHorizontalAlignment(JTextField.CENTER);
+        xInput.setAlignmentY(JTextField.CENTER);
+        sidepanel.add(xInput);
+
+        JLabel yText = new JLabel("y: ");
+        yText.setHorizontalTextPosition(JLabel.CENTER);
+        yText.setVerticalTextPosition(JLabel.TOP);
+        yText.setFont(new Font("Ubuntu", Font.BOLD, 15));
+        yText.setPreferredSize(new Dimension(100, 30));
+        sidepanel.add(yText);
+
+        yInput = new JTextField();
+        yInput.setMaximumSize(new Dimension(100, 30));
+        yInput.setPreferredSize(new Dimension(100,20));
+        yInput.setFont(new Font("Ubuntu", Font.PLAIN, 12));
+        yInput.setHorizontalAlignment(JTextField.CENTER);
+        yInput.setAlignmentY(JTextField.CENTER);
+        sidepanel.add(yInput);
+
+        JLabel deltaText = new JLabel("delta: ");
+        deltaText.setHorizontalTextPosition(JLabel.CENTER);
+        deltaText.setVerticalTextPosition(JLabel.TOP);
+        deltaText.setFont(new Font("Ubuntu", Font.BOLD, 15));
+        deltaText.setPreferredSize(new Dimension(100, 30));
+        sidepanel.add(deltaText);
+
+        deltaInput = new JTextField();
+        deltaInput.setMaximumSize(new Dimension(100, 30));
+        deltaInput.setPreferredSize(new Dimension(100,20));
+        deltaInput.setFont(new Font("Ubuntu", Font.PLAIN, 12));
+        deltaInput.setHorizontalAlignment(JTextField.CENTER);
+        deltaInput.setAlignmentY(JTextField.CENTER);
+        sidepanel.add(deltaInput);
+
+        JLabel vText = new JLabel("delta: ");
+        vText.setHorizontalTextPosition(JLabel.CENTER);
+        vText.setVerticalTextPosition(JLabel.TOP);
+        vText.setFont(new Font("Ubuntu", Font.BOLD, 15));
+        vText.setPreferredSize(new Dimension(100, 30));
+        sidepanel.add(vText);
+
+        vInput = new JTextField();
+        vInput.setMaximumSize(new Dimension(100, 30));
+        vInput.setPreferredSize(new Dimension(100,20));
+        vInput.setFont(new Font("Ubuntu", Font.PLAIN, 12));
+        vInput.setHorizontalAlignment(JTextField.CENTER);
+        vInput.setAlignmentY(JTextField.CENTER);
+        sidepanel.add(vInput);
+
+        submitParticle = new JButton("Submit Particle");
+        submitParticle.addActionListener(this);
+        sidepanel.add(submitParticle);
     }
 
     public void toggleVisibility(){
         window.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource() == submitParticle){
+            System.out.println(xInput.getText());
+            System.out.println(yInput.getText());
+            System.out.println(deltaInput.getText());
+            System.out.println(vInput.getText());
+        }
+        else if (e.getSource() == submitWall){
+
+
+
+        }
     }
 
 }
